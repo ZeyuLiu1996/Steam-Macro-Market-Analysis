1702ed47fba09087640a9377020c59b2a59944f3
ZeyuLiu1996 <liu553869360@gmail.com>
2024-05-14 17:29:45 +0800

    Update using steamAPI script
    1.Update 'use_steam_api.ipynb' script
    2.Adjust the ‘ids_for_api_use.csv' file, add a judgment column

--- src/data_extraction/steam_ids_for_api_use.sql
+++ src/data_extraction/steam_ids_for_api_use.sql
@@ -1,7 +1,11 @@
+/*
+结束时请您将结果保存为CSV文件至'data\raw\ids_for_api_use.csv'，以便供后续分析
+*/
+
 DECLARE @jsonContent NVARCHAR(MAX)
 
 SELECT @jsonContent = BulkColumn --将数据变成一大块
-FROM OPENROWSET (BULK 'C:\Users\55386\OneDrive\Documents\Scripts\Projects\Steam_DA\data\raw\steam_datas.json', SINGLE_CLOB) AS j --这里请替换为您自己所保存的路径
+FROM OPENROWSET (BULK 'C:\Users\55386\OneDrive\Documents\Scripts\Projects\Steam_DA\data\raw\steam_datas.json', SINGLE_CLOB) AS j --这里请替换为您自己所保存的路径，此处使用的是作者的本地路径
 
 SELECT DISTINCT steamid_object.[key]
 FROM OPENJSON(@jsonContent) AS steamid_object --解析JSON格式
